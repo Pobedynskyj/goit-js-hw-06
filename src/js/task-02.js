d7a527504c8c2f2eb8c5a70e18b49a32397d869c
Pobedynskyj Volodymyr <punkglue1@gmail.com>
2022-09-27 18:46:44 +0300

    task 2 okay

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,31 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const navItem = document.querySelector(`ul`);
+console.log(navItem);
+
+const navItemElPot = document.createElement(`li`);
+navItemElPot.classList.add(`item`);
+navItemElPot.textContent = `Potatoes`;
+
+const navItemElMush = document.createElement(`li`);
+navItemElMush.classList.add(`item`);
+navItemElMush.textContent = `Mushrooms`;
+
+const navItemElGarl = document.createElement(`li`);
+navItemElGarl.classList.add(`item`);
+navItemElGarl.textContent = `Garlic`;
+
+const navItemElTom = document.createElement(`li`);
+navItemElTom.classList.add(`item`);
+navItemElTom.textContent = `Tomatos`;
+
+const navItemElHerbs = document.createElement(`li`);
+navItemElHerbs.classList.add(`item`);
+navItemElHerbs.textContent = `Herbs`;
+
+const navItemElCond = document.createElement(`li`);
+navItemElCond.classList.add(`item`);
+navItemElCond.textContent = `Condiments`;
+
+navItem.append(navItemElPot, navItemElMush, navItemElGarl, navItemElTom, navItemElHerbs, navItemElCond);
