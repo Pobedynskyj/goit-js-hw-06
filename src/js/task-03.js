0b8e37a380c18a1e4ba5818858b3baedb42de0a5
Pobedynskyj Volodymyr <punkglue1@gmail.com>
2022-09-27 22:13:45 +0300

    task 3 okay

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -12,3 +12,11 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+const galleryEl = document.querySelector(`.gallery`);
+console.log(galleryEl);
+
+const markup = images.map(image => {
+  return `<li><img src = "${image.url}" alt = "${image.alt}"`
+}).join(``)
+console.log(markup);
+galleryEl.insertAdjacentHTML(`beforeend`, markup);
