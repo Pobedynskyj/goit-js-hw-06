3b7c15fad747036842d9ed6096a60aa8fa8fcf4a
Pobedynskyj Volodymyr <punkglue1@gmail.com>
2022-09-30 21:53:20 +0300

    task 6 ok

---
+++ src/js/task-06.js
@@ -0,0 +1,13 @@
+const validInputRef = document.querySelector("#validation-input");
+validInputRef.addEventListener("blur", onInputBlur);
+
+function onInputBlur() {
+  if (validInputRef.value.length === Number(validInputRef.dataset.length)) {
+    validInputRef.classList.add("valid");
+    // validInputRef.classList.remove("invalid");
+    return;
+  } else {
+    validInputRef.classList.remove("valid");
+    validInputRef.classList.add("invalid");
+  }
+}
