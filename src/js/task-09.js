b592118b2fc8c42b7cef51eeb81efcc0300d0e51
Pobedynskyj Volodymyr <punkglue1@gmail.com>
2022-10-03 23:15:24 +0300

    task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const bodyEl = document.querySelector(`body`);
+const buttonEl = document.querySelector(`.change-color`);
+const colorIdEl = document.querySelector(`.color`);
+
+buttonEl.addEventListener("click", (event) => {
+  const random = getRandomHexColor();
+  bodyEl.style.backgroundColor = random;
+  colorIdEl.textContent = random;
+});
