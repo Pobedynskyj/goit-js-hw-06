9a2784a40f8f579f930c13b3256c5416781941c3
Pobedynskyj Volodymyr <punkglue1@gmail.com>
2022-10-03 22:44:44 +0300

    task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,13 @@
-
+const formEl = document.querySelector(`.login-form`);
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    alert("Всі поля повинні бути заповнені!");
+  } else {
+    console.log({ email: email.value, password: password.value });
+    formEl.reset();
+  }
+});
