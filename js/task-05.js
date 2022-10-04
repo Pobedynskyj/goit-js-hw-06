b3ac856212678c1cc341480b4df4d65c9ea09438
Pobedynskyj Volodymyr <punkglue1@gmail.com>
2022-10-04 21:14:41 +0300

    task5 update

--- js/task-05.js
+++ js/task-05.js
@@ -2,4 +2,7 @@ let nameIn = document.querySelector(`#name-input`);
 let nameOut = document.querySelector(`#name-output`);
 nameIn.addEventListener("input", (event) => {
   nameOut.textContent = event.currentTarget.value;
+  if (event.currentTarget.value === "") {
+    nameOut.textContent = "Anonymous";
+  }
 });
